3ef3a3f70bc7ee04c41d8916d1dfef4a985e0023
patopnp <54560021+patopnp@users.noreply.github.com>
2019-11-04 13:00:16 -0300

    Add files via upload
    Se agregaron los usuarios, logins y permisos en un esquema nuevo

---
+++ Desarrollo/03 Usuarios.sql
@@ -0,0 +1,17 @@
+-- Creamos el esquema y otorgamos los permisos
+CREATE SCHEMA Monitoreo;
+GRANT ALTER ON Parametros SCHEMA::Monitoreo TO admin;
+GRANT INSERT ON Metrica SCHEMA::Monitoreo TO usuarioActualizacion;
+GRANT SELECT ON SCHEMA::Monitoreo TO lector;
+
+
+CREATE LOGIN adminLogin WITH PASSWORD = 'admin1234';
+CREATE USER admin FOR LOGIN adminLogin WITH DEFAULT_SCHEMA = Monitoreo;
+
+
+CREATE LOGIN usuarioActualizacionLogin WITH PASSWORD = 'usuario1234';
+CREATE USER usuarioActualizacion FOR LOGIN usuarioActualizacionLogin WITH DEFAULT_SCHEMA = Monitoreo;
+
+
+CREATE LOGIN lectorLogin WITH PASSWORD = 'lector1234';
+CREATE USER lector FOR LOGIN lectorLogin WITH DEFAULT_SCHEMA = Monitoreo;
